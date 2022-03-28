a8d8f27f029d6b8f09865ab1d480484dee9f65b4
seoyeon-baek <w2032@e-mirim.hs.kr>
2022-03-28 09:42:22 +0900

    pull

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,74 +1,61 @@
-<<<<<<< Updated upstream
-import React, { useMemo, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
-import products from "../../data/products.json";
-import useStore from "../../store";
-import { Container, Button } from "@mui/material";
-import { useScrollPosition, ScrollProps } from "../../hooks/useScrollPosition";
-=======
-import React, {useEffect} from "react";
+import React, {useMemo, useEffect, useRef} from "react";
 import {Link} from "react-router-dom";
 import products from "../../data/products.json";
 import useStore from "../../store";
 import {Container, Button} from "@mui/material";
->>>>>>> Stashed changes
+import {useScrollPosition, ScrollProps} from "../../hooks/useScrollPosition";
 
 const HomePage = () => {
   const store = useStore();
   const [rad, setRad] = React.useState(0);
-  useScrollPosition(({ prevPos, currPos }: ScrollProps) => {
-    const isChange = currPos?.y !== prevPos?.y;
-    if (isChange) setRad(-currPos.y / 600 % Math.PI);
-
-  }, [rad]);
+  useScrollPosition(
+    ({prevPos, currPos}: ScrollProps) => {
+      const isChange = currPos?.y !== prevPos?.y;
+      if (isChange) setRad((-currPos.y / 600) % Math.PI);
+    },
+    [rad]
+  );
 
   useEffect(() => {
     console.log(store.counter);
     store.addCounter();
-    
   }, []);
 
-
-
-
-  return useMemo(() => (
-
-    <div>
-<<<<<<< Updated upstream
-      <div style={styles.center_logo}>
-        <div style={styles.center_logo_inner}>
-          <Link to="/" >
-            <img src="/images/full_logo.svg" alt="Non_Fiction_Store" style={{ transform: `rotate(${rad}rad)` }} />
-          </Link>
-        </div>
-
-      </div>
-
-      <Container style={styles.page}>
-        {products.map((product) => (
-=======
-      <Container>
-        {products.map(product => (
->>>>>>> Stashed changes
-          <Button key={product.id}>
-            <Link to={`/product/${product.id}`}>
+  return useMemo(
+    () => (
+      <div>
+        <div style={styles.center_logo}>
+          <div style={styles.center_logo_inner}>
+            <Link to="/">
               <img
-                src={product.image}
-                alt={product.name}
-                height="200"
-                width="200"
+                src="/images/full_logo.svg"
+                alt="Non_Fiction_Store"
+                style={{transform: `rotate(${rad}rad)`}}
               />
             </Link>
-          </Button>
-        ))}
-      </Container>
+          </div>
+        </div>
 
-      <div style={styles.footer}>
-        footer
+        <Container>
+          {products.map(product => (
+            <Button key={product.id}>
+              <Link to={`/product/${product.id}`}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  height="200"
+                  width="200"
+                />
+              </Link>
+            </Button>
+          ))}
+        </Container>
+
+        <div style={styles.footer}>footer</div>
       </div>
-    </div >
-
-  ), [rad]);
+    ),
+    [rad]
+  );
 };
 
 const styles = {
@@ -77,7 +64,6 @@ const styles = {
     height: "100%",
     overflowY: "auto",
     overflowX: "hidden",
-
   },
   center_logo: {
     position: "fixed",
@@ -99,7 +85,7 @@ const styles = {
   },
   page: {
     position: "relative",
-    top: "10vh"
+    top: "10vh",
   },
   footer: {
     position: "relative",
@@ -107,8 +93,7 @@ const styles = {
     left: "50%",
     right: "0",
     top: "2000px",
-
-  }
-} as const
+  },
+} as const;
 
 export default HomePage;
